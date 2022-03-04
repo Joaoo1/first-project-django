e730ff4d933b779d5055e15ff44a5e3001f7e996
João Vitor da Silva <silva.joao1@outlook.com.br>
2022-03-03 21:41:26 -0300

    feat: add static files

--- core/views.py
+++ core/views.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
 
+from .models import Product
 
 # Create your views here.
 def index(request):
+    products = Product.objects.all()
+
     context = {
-        'curso': 'Curso Teste 1'
+        'curso': 'Curso Teste 1',
+        'products': products
     }
+
     return render(request, 'index.html', context)
 
 
 def contact(request):
     return render(request, 'contact.html')
+
+    
+def product(request, id):
+    # prod = Product.objects.get(id=id)
+    prod = get_object_or_404(Product, id=id)
+
+    context = {
+        'product': prod
+    }
+    return render(request, 'product.html', context)
+
+def error404(request, ex):
+    template = loader.get_template('404.html')
+    return HttpResponse(content=template.render(), content_type="text/html; charset=utf8", status=404)
+
+def error500(request):
+    template = loader.get_template('500.html')
+    return HttpResponse(content=template.render(), content_type="text/html; charset=utf8", status=500)
--- firstproject/settings.py
+++ firstproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-#rs-oluvbf6h2khbz%!vewz8w%h2xfg6mc%4eg*6=!ko168(f$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,8 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'index'
